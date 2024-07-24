c357b606ce0ff5b0f430975a30dedc7baf55f236
BD24K <bryanjosueflores24@gmail.com>
2024-07-24 07:52:01 -0600

    arreglo componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { NavComponentComponent } from './nav/nav-component/nav-component.component';
+import { FooterComponentComponent } from './footer/footer-component/footer-component.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavComponentComponent,FooterComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
