e42711687bb1512ff6b3ef6f8d67f067072c7e7f
JosueFlores777 <bryanjosueflores24@gmail.com>
2024-08-10 08:17:10 -0600

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -64,6 +64,57 @@ export class AppComponent {
     ]
   };
 
+
+  logos = [
+    /*'./assets/img/logo/',*/
+    'https://media.istockphoto.com/id/1337232523/es/foto/vista-de-ángulo-alto-de-un-lago-y-un-bosque.jpg?s=2048x2048&w=is&k=20&c=ohPNTFlDn-ARAawgnbCzIOzWMadL82fUe6qc92Los_k=',
+    'https://media.istockphoto.com/id/1337232523/es/foto/vista-de-ángulo-alto-de-un-lago-y-un-bosque.jpg?s=2048x2048&w=is&k=20&c=ohPNTFlDn-ARAawgnbCzIOzWMadL82fUe6qc92Los_k=',
+    'https://media.istockphoto.com/id/1337232523/es/foto/vista-de-ángulo-alto-de-un-lago-y-un-bosque.jpg?s=2048x2048&w=is&k=20&c=ohPNTFlDn-ARAawgnbCzIOzWMadL82fUe6qc92Los_k=',
+    'https://media.istockphoto.com/id/1337232523/es/foto/vista-de-ángulo-alto-de-un-lago-y-un-bosque.jpg?s=2048x2048&w=is&k=20&c=ohPNTFlDn-ARAawgnbCzIOzWMadL82fUe6qc92Los_k=',
+    'https://media.istockphoto.com/id/1337232523/es/foto/vista-de-ángulo-alto-de-un-lago-y-un-bosque.jpg?s=2048x2048&w=is&k=20&c=ohPNTFlDn-ARAawgnbCzIOzWMadL82fUe6qc92Los_k=',
+    'https://media.istockphoto.com/id/1337232523/es/foto/vista-de-ángulo-alto-de-un-lago-y-un-bosque.jpg?s=2048x2048&w=is&k=20&c=ohPNTFlDn-ARAawgnbCzIOzWMadL82fUe6qc92Los_k=',
+    'https://media.istockphoto.com/id/1337232523/es/foto/vista-de-ángulo-alto-de-un-lago-y-un-bosque.jpg?s=2048x2048&w=is&k=20&c=ohPNTFlDn-ARAawgnbCzIOzWMadL82fUe6qc92Los_k=',
+    'https://media.istockphoto.com/id/1337232523/es/foto/vista-de-ángulo-alto-de-un-lago-y-un-bosque.jpg?s=2048x2048&w=is&k=20&c=ohPNTFlDn-ARAawgnbCzIOzWMadL82fUe6qc92Los_k=',
+    'https://media.istockphoto.com/id/1337232523/es/foto/vista-de-ángulo-alto-de-un-lago-y-un-bosque.jpg?s=2048x2048&w=is&k=20&c=ohPNTFlDn-ARAawgnbCzIOzWMadL82fUe6qc92Los_k='
+    
+  ];
+
+  slideConfig2 = {
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 0,
+    infinite: true,
+    speed: 4000,
+    cssEase: 'linear',
+    arrows: false,
+    dots: false,
+    responsive: [
+      {
+        breakpoint: 1024, 
+        settings: {
+          slidesToShow: 3, 
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 768, 
+        settings: {
+          slidesToShow: 2, 
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480, 
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
+  
+
   trackByFn(index: number, item: any): number {
     return index;
   }
