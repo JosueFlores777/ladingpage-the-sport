0a41e0efe530e02232091a857c91824b41117a97
JosueFlores777 <bryanjosueflores24@gmail.com>
2024-08-20 14:59:27 -0600

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,22 +17,22 @@ export class AppComponent {
 
   comercios = [
     { nombre: 'Papa Johns', img: './assets/img/content/picture.jpg', descripcion: 'Mejores ingredientes, mejor pizza.', link: 'https://www.instagram.com/papajohns_sv/' },
-    { nombre: 'Mak Meats', img: './assets/img/content/picture-01.jpg', descripcion: '🔥Tenemos las carnes, mariscos, papas y más que necesitas en tu negocio.', link: '#' },
-    { nombre: 'Banco Atlantida', img: './assets/img/content/picture-02.jpg', descripcion: 'Una institución centenaria con trayectoria regional.', link: '#' },
-    { nombre: 'El Sin Rival', img: './assets/img/content/picture-03.jpg', descripcion: '¡Sorbetes NATURALES! 🍨👌😉', link: '#' },
-    { nombre: 'Farmacias Camila', img: './assets/img/content/picture 05.jpg', descripcion: 'Nuestro compromiso es TU SALUD💚', link: '#' },
-    { nombre: 'Bodeganga Hilasal', img: './assets/img/content/picture 06.jpg', descripcion: 'Toallas Hilasal con precios especiales al mayoreo y al detalle.', link: '#' },
-    { nombre: 'Soya Nutribar', img: './assets/img/content/picture 07.jpg', descripcion: 'Comida saludable, naturalmente deliciosa.', link: '#' },
-    { nombre: 'Cabal Internet', img: './assets/img/content/picture 08.jpg', descripcion: 'Proveedor de servicios de internet', link: '#' },
-    { nombre: 'MyCeviche', img: './assets/img/content/picture 09.jpg', descripcion: 'En My Ceviche te preparamos nuestro Ceviche Original perfecto hecho con la salsa especial del chef🤤', link: '#' },
-    { nombre: 'TenGo Market', img: './assets/img/content/picture 10.jpg', descripcion: 'Hacemos que cada día sea más fácil y mejor!', link: '#' },
-    { nombre: 'Cheers', img: './assets/img/content/picture 11.jpg', descripcion: 'Boutique de Licores, Vinos & Deli', link: '#' },
-    { nombre: 'Porta Real Estate', img: './assets/img/content/picture 12.jpg', descripcion: 'Lo mejor del Real Estate en El Salvador ya esta en Surf City 🏄‍♂️.', link: '#' },
-    { nombre: 'La Neveria', img: './assets/img/content/picture 13.jpg', descripcion: '"Siempre Algo Nuevo" Helados, paletas, pasteles de helado.', link: '#' },
-    { nombre: 'Movistar', img: './assets/img/content/picture 14.jpg', descripcion: 'Telefonía móvil y fija, Internet, celulares, promociones y más.', link: '#' },
-    { nombre: 'Castella Sagarra', img: './assets/img/content/picture 15.jpg', descripcion: 'Siempre en ruta! 🐷🚗 ES 📍 Los mejores chicharrones de C.A.', link: '#' },
-    { nombre: 'Seaside Sourdough', img: './assets/img/content/picture 16.jpg', descripcion: 'Imperdiet purus pellentesque sit mi nibh sit integer faucibus.', link: '#' }
-  ];
+    { nombre: 'Mak Meats', img: './assets/img/content/picture-01.jpg', descripcion: '🔥Tenemos las carnes, mariscos, papas y más que necesitas en tu negocio.', link: 'https://www.instagram.com/makmeats.sv/?hl=es' },
+    { nombre: 'Banco Atlantida', img: './assets/img/content/picture-02.jpg', descripcion: 'Una institución centenaria con trayectoria regional.', link: 'https://www.instagram.com/bancatlan/?hl=es' },
+    { nombre: 'El Sin Rival', img: './assets/img/content/picture-03.jpg', descripcion: '¡Sorbetes NATURALES! 🍨👌😉', link: 'https://www.instagram.com/elsinrivaloficial/?hl=es' },
+    { nombre: 'Farmacias Camila', img: './assets/img/content/picture 05.jpg', descripcion: 'Nuestro compromiso es TU SALUD💚', link: 'https://www.instagram.com/farmaciascamilasv/?hl=es' },
+    { nombre: 'Bodeganga Hilasal', img: './assets/img/content/picture 06.jpg', descripcion: 'Toallas Hilasal con precios especiales al mayoreo y al detalle.', link: 'https://www.instagram.com/bodegangashilasal/?hl=es' },
+    { nombre: 'Soya Nutribar', img: './assets/img/content/picture 07.jpg', descripcion: 'Comida saludable, naturalmente deliciosa.', link: 'https://www.instagram.com/soyanutribar/?hl=es' },
+    { nombre: 'Cabal Internet', img: './assets/img/content/picture 08.jpg', descripcion: 'Proveedor de servicios de internet', link: 'https://www.facebook.com/cabalinternet/' },
+    { nombre: 'MyCeviche', img: './assets/img/content/picture 09.jpg', descripcion: 'En My Ceviche te preparamos nuestro Ceviche Original perfecto hecho con la salsa especial del chef🤤', link: 'https://www.instagram.com/myceviche/?hl=es' },
+    { nombre: 'TenGo Market', img: './assets/img/content/picture 10.jpg', descripcion: 'Hacemos que cada día sea más fácil y mejor!', link: 'https://www.instagram.com/tengo.todomarket/?hl=es' },
+    { nombre: 'Cheers', img: './assets/img/content/picture 11.jpg', descripcion: 'Boutique de Licores, Vinos & Deli', link: 'https://www.instagram.com/cheers.sv/?hl=es' },
+    { nombre: 'Porta Real Estate', img: './assets/img/content/picture 12.jpg', descripcion: 'Lo mejor del Real Estate en El Salvador ya esta en Surf City 🏄‍♂️.', link: 'https://www.instagram.com/portarealestate/?hl=es' },
+    { nombre: 'La Neveria', img: './assets/img/content/picture 13.jpg', descripcion: '"Siempre Algo Nuevo" Helados, paletas, pasteles de helado.', link: 'https://www.instagram.com/laneveria_oficial/?hl=es' },
+    { nombre: 'Movistar', img: './assets/img/content/picture 14.jpg', descripcion: 'Telefonía móvil y fija, Internet, celulares, promociones y más.', link: 'https://www.instagram.com/movistar_sv/?hl=es' },
+    { nombre: 'Don Chicharron', img: './assets/img/content/picture 15.jpg', descripcion: 'Siempre en ruta! 🐷🚗 ES 📍 Los mejores chicharrones de C.A.', link: 'https://www.instagram.com/donchicharron.sv/?hl=es' },
+    { nombre: 'Seaside Sourdough', img: './assets/img/content/picture 16.jpg', descripcion: 'Imperdiet purus pellentesque sit mi nibh sit integer faucibus.', link: 'https://www.instagram.com/seaside.sourdough/' }
+];
   slideConfig = {
     slidesToShow: 4,
     slidesToScroll: 1,
@@ -66,18 +66,29 @@ export class AppComponent {
 
 
   logos = [
-    /*'./assets/img/logo/',*/
-    'https://media.istockphoto.com/id/1337232523/es/foto/vista-de-ángulo-alto-de-un-lago-y-un-bosque.jpg?s=2048x2048&w=is&k=20&c=ohPNTFlDn-ARAawgnbCzIOzWMadL82fUe6qc92Los_k=',
-    'https://media.istockphoto.com/id/1337232523/es/foto/vista-de-ángulo-alto-de-un-lago-y-un-bosque.jpg?s=2048x2048&w=is&k=20&c=ohPNTFlDn-ARAawgnbCzIOzWMadL82fUe6qc92Los_k=',
-    'https://media.istockphoto.com/id/1337232523/es/foto/vista-de-ángulo-alto-de-un-lago-y-un-bosque.jpg?s=2048x2048&w=is&k=20&c=ohPNTFlDn-ARAawgnbCzIOzWMadL82fUe6qc92Los_k=',
-    'https://media.istockphoto.com/id/1337232523/es/foto/vista-de-ángulo-alto-de-un-lago-y-un-bosque.jpg?s=2048x2048&w=is&k=20&c=ohPNTFlDn-ARAawgnbCzIOzWMadL82fUe6qc92Los_k=',
-    'https://media.istockphoto.com/id/1337232523/es/foto/vista-de-ángulo-alto-de-un-lago-y-un-bosque.jpg?s=2048x2048&w=is&k=20&c=ohPNTFlDn-ARAawgnbCzIOzWMadL82fUe6qc92Los_k=',
-    'https://media.istockphoto.com/id/1337232523/es/foto/vista-de-ángulo-alto-de-un-lago-y-un-bosque.jpg?s=2048x2048&w=is&k=20&c=ohPNTFlDn-ARAawgnbCzIOzWMadL82fUe6qc92Los_k=',
-    'https://media.istockphoto.com/id/1337232523/es/foto/vista-de-ángulo-alto-de-un-lago-y-un-bosque.jpg?s=2048x2048&w=is&k=20&c=ohPNTFlDn-ARAawgnbCzIOzWMadL82fUe6qc92Los_k=',
-    'https://media.istockphoto.com/id/1337232523/es/foto/vista-de-ángulo-alto-de-un-lago-y-un-bosque.jpg?s=2048x2048&w=is&k=20&c=ohPNTFlDn-ARAawgnbCzIOzWMadL82fUe6qc92Los_k=',
-    'https://media.istockphoto.com/id/1337232523/es/foto/vista-de-ángulo-alto-de-un-lago-y-un-bosque.jpg?s=2048x2048&w=is&k=20&c=ohPNTFlDn-ARAawgnbCzIOzWMadL82fUe6qc92Los_k='
-    
-  ];
+    './assets/img/logo/agua-inmaculada-logo.png',
+    './assets/img/logo/banco-atlantida.png',
+    './assets/img/logo/Logo Cabal.png',
+    './assets/img/logo/Logo La Neveria.png',
+    './assets/img/logo/hilasal-logo.png',
+    './assets/img/logo/Movistar_logo.png',
+    './assets/img/logo/MyCevi logox2.png',
+    './assets/img/logo/Papa Johns Wordmark.png',
+    './assets/img/logo/PORTA_logo.png',
+    './assets/img/logo/soya-nutribar-logo.png',
+    './assets/img/logo/tengo-todo-market-logo.png'
+    ,    './assets/img/logo/agua-inmaculada-logo.png',
+    './assets/img/logo/banco-atlantida.png',
+    './assets/img/logo/Logo Cabal.png',
+    './assets/img/logo/Logo La Neveria.png',
+    './assets/img/logo/hilasal-logo.png',
+    './assets/img/logo/Movistar_logo.png',
+    './assets/img/logo/MyCevi logox2.png',
+    './assets/img/logo/Papa Johns Wordmark.png',
+    './assets/img/logo/PORTA_logo.png',
+    './assets/img/logo/soya-nutribar-logo.png',
+    './assets/img/logo/tengo-todo-market-logo.png'
+];
 
   slideConfig2 = {
     slidesToShow: 5,
@@ -85,13 +96,21 @@ export class AppComponent {
     autoplay: true,
     autoplaySpeed: 0,
     infinite: true,
-    speed: 4000,
+    speed: 5000, 
     cssEase: 'linear',
     arrows: false,
     dots: false,
+    pauseOnHover: false,
     responsive: [
       {
         breakpoint: 1024, 
+        settings: {
+          slidesToShow: 4.5, 
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 968, 
         settings: {
           slidesToShow: 3, 
           slidesToScroll: 1
@@ -107,7 +126,7 @@ export class AppComponent {
       {
         breakpoint: 480, 
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 1.3,
           slidesToScroll: 1
         }
       }
